d21bbe67bc1db5ee169004f5c634958aa88e6d23
Sami Jaber <jabersami@gmail.com>
2023-04-18 17:08:59 -0300

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,12 @@
-import { defineConfig } from 'vite'
-import { createVuePlugin } from 'vite-plugin-vue2'
+import { defineConfig } from "vite";
+import createVuePlugin from "@vitejs/plugin-vue2";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/',
-  plugins: [createVuePlugin({ jsx: true })],
+  base: "/",
+  plugins: [createVuePlugin()],
   server: {
     open: true,
     port: 8002,
   },
-})
+});
